cec9a199a9aeacb90a3b1f25023ded6be3c0c740
Tommy Mai <tommymaicollege@gmail.com>
2023-01-16 17:32:54 -0800

    Login Form

--- ghi/src/LoginForm.js
+++ ghi/src/LoginForm.js
@@ -1,83 +1,48 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { NavLink } from "react-router-dom";
-import { useToken } from "./auth";
+import { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
-function LoginForm() {
-  const [, login] = useToken();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [invalid, setInvalid] = useState(false);
-  let navigate = useNavigate();
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const history = useHistory();
 
-  const clearState = () => {
-    setEmail("");
-    setPassword("");
-    setInvalid("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // Make a request to authenticate the user
+      const data = await authenticateUser(email, password);
+      // If authentication is successful
+      if (data.success) {
+        history.push('/main');
+      } else {
+        setError(data.message);
+      }
+    } catch (err) {
+      setError('There was an error logging in. Please try again.');
+    }
   };
 
-  async function handleSubmit(e) {
-    e.preventDefault();
-    const error = await login(email, password);
-    if (error) {
-      setInvalid(true);
-    } else {
-      clearState();
-      navigate("/");
-    }
-  }
-
   return (
-    <div className="row">
-      <div className="offset-3 col-6">
-        <div className="shadow p-4 mt-4">
-          <h1 className="text-left display-6">Login</h1>
-          <div className="text-left mb-3 lead">
-            Don't have an account?
-            <NavLink to="/signup"> Create account</NavLink>
-          </div>
-          <form id="create-appointment-form" onSubmit={handleSubmit}>
-            <div className="form-floating mb-3">
-              <input
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="name@example.com"
-                required
-                type="text"
-                name="email"
-                id="email"
-                className="form-control"
-              />
-              <label htmlFor="email">Email</label>
-            </div>
-            <div className="form-floating mb-3">
-              <input
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="password"
-                required
-                type="password"
-                name="password"
-                id="password"
-                className="form-control"
-              />
-              <label htmlFor="password">Password</label>
-            </div>
-            <div className="col text-left">
-              <button className="btn btn-primary">Log In</button>
-            </div>
-          </form>
-          {invalid && (
-            <div
-              className="alert alert-danger mb-0 p-4 mt-4"
-              id="invalid-message"
-            >
-              Incorrect email and/or password. Please
-              try again.
-            </div>
-          )}
-        </div>
-      </div>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+        required
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+        required
+      />
+      <button type="submit">Login</button>
+      {error && <p>{error}</p>}
+    </form>
   );
-}
+};
 
 export default LoginForm;
