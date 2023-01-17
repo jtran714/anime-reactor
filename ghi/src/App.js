014aad681ea3398a291b1045c5c5fee552a7b6a4
Tommy Mai <tommymaicollege@gmail.com>
2023-01-17 15:33:43 -0800

    Updates

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,14 +1,52 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Construct from "./Construct.js";
+import ErrorNotification from "./ErrorNotification";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./Nav";
-import SignupForm from "./SignupForm";
+import SignUpForm from "./SignUpForm.js";
+import EditAccount from "./AccountEdit.js";
+import LoginForm from "./LoginForm.js";
+import "./App.css";
+
 
 function App() {
+  // const [launch_info, setLaunchInfo] = useState([]);
+  // const [error, setError] = useState(null);
+
+  // useEffect(() => {
+  //   async function getData() {
+  //     let url = `${process.env.REACT_APP_ACCOUNTS}/api/launch-details`;
+  //     console.log("fastapi url: ", url);
+  //     let response = await fetch(url);
+  //     console.log("------- hello? -------");
+  //     let data = await response.json();
+
+  //     if (response.ok) {
+  //       console.log("got launch data!");
+  //       setLaunchInfo(data.launch_details);
+  //     } else {
+  //       console.log("drat! something happened");
+  //       setError(data.message);
+  //     }
+  //   }
+  //   getData();
+  // }, []);
+
   return (
-    <BrowserRouter>
-      <Nav />
-      <SignupForm />
-    </BrowserRouter>
+    <div>
+      {/* <ErrorNotification error={error} />
+      <Construct info={launch_info} /> */}
+      <BrowserRouter>
+        <Nav />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<SignUpForm />} />
+                        <Route path="/SignUpForm" element={<SignUpForm />} />
+            <Route path="/SignUpForm" element={<SignUpForm />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
   );
 }
 
