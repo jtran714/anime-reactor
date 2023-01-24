d913294d04895e0a134efddac38210319d0a8449
Chengyun Lee <chengyun.lee2015@gmail.com>
2023-01-24 12:54:13 -0800

    remove old file

--- ghi/src/watchlist.js
+++ ghi/src/watchlist.js
@@ -1,97 +1,107 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
+import { useAuthContext } from "./auth";
 
-const Watchlist = () => {
-  const [watchlists, setWatchlists] = useState([]);
-  const [submitted, setSubmitted] = useState(false);
 
-  // First argument: state variable
-  // Second argument: function
+export default function Watchlist() {
+    const { token } = useAuthContext();
+    const [anime, setAnime] = useState([]);
+    const [decodedUser, setDecodedUser] = useState("")
+    const [submitted, setSubmitted] = useState(false);
 
-  const user = {
-    id: "1",
-  };
-  const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlN2UyYmJlMS05Njc5LTQ1NWQtOTQ4MC0zYTJjZTBmM2ExZTUiLCJleHAiOjE2NzQxNjAxMDIsInN1YiI6InN0cmluZyIsImFjY291bnQiOnsiaWQiOjEsImZpcnN0X25hbWUiOiJzdHJpbmciLCJsYXN0X25hbWUiOiJzdHJpbmciLCJlbWFpbCI6InN0cmluZyIsInVzZXJuYW1lIjoic3RyaW5nIn19.LxWLhQ0cPDvoSghMEdg2cUU1BWhbi9fNLaVZqNi3yZ0";
+    function parseJwt(token) {
+      console.log(token);
+      let base64Url = token.split(".")[1];
+      let base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+      let jsonPayload = decodeURIComponent(
+        window
+          .atob(base64)
+          .split("")
+          .map(function (c) {
+            return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+          })
+          .join("")
+      );
+      const variable = JSON.parse(jsonPayload);
+      setDecodedUser(variable.account);
+    }
 
-  useEffect(() => {
-    async function getWatchlist() {
-      const watchlistURL = `${process.env.REACT_APP_WATCHLISTS_API_HOST}/api/watchlists/${user.id}`;
-      const fetchConfig = {
-        method: "GET",
-        headers: {
-          Authentication: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-      };
-      const response = await fetch(watchlistURL, fetchConfig);
-      if (response.ok) {
-        const data = response.json();
-        setWatchlists(data.watchlists);
-      } else {
-        console.log("fetch error");
+    useEffect(() => {
+      if (token !== null) {
+        parseJwt(token)
       }
-    }
-    getWatchlist();
-  }, [watchlists, user.id, submitted]);
+    }, [token]);
 
-  const delWatchlist = async (watchlist) => {
-    const delURL = `${process.env.REACT_APP_WATCHLISTS_API_HOST}/watchlists/${user.id}/${watchlist.id}`;
-    const fetchConfig = {
-      method: "DELETE",
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
+    useEffect(() => {
+      console.log(decodedUser, "============decodedUser============");
+        async function getWatchlist() {
+            const watchlistsURL = `${process.env.REACT_APP_WATCHLISTS_API_HOST}/api/watchlists/${decodedUser.id}`;
+            const fetchConfig = {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                },
+            };
+
+            const response = await fetch(watchlistsURL, fetchConfig);
+            if (response.ok) {
+                const data = await response.json();
+                setAnime(data.watchlists);
+            }
+        }
+        if (decodedUser) {
+            getWatchlist();
+        }
+    }, [token, decodedUser, submitted]);
+
+    const removeWatchlist = async (watchlist_id) => {
+        const removeURL = `${process.env.REACT_APP_WATCHLISTS_API_HOST}/api/watchlists/${decodedUser.id}/${watchlist_id}`;
+        const fetchConfig = {
+            method: "delete",
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json",
+            },
+        };
+        const response = await fetch (removeURL, fetchConfig);
+        if (response.ok) {
+            setAnime([...anime]);
+            setSubmitted(true);
+        }
     };
-    const response = await fetch(delURL, fetchConfig);
-    if (response.ok) {
-      setWatchlists([...watchlist]);
-      setSubmitted(true);
-    }
-
-    // function parseJwt (token) {
-    //     var base64Url = token.split('.')[1];
-    //     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    //     var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-    //         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    //     }).join(''));
-
-    //     return JSON.parse(jsonPayload);
-    // }
 
     return (
       <>
-        <h1>Anime in Your Watch List</h1>
+        <h1>Anime in your watchlist</h1>
         <table className="table table-dark table-striped">
           <thead>
             <tr>
               <th>Anime Title</th>
-              <th>Date</th>
+              <th>Air Date</th>
               <th>Picture</th>
               <th>Edit</th>
             </tr>
           </thead>
           <tbody>
-            {watchlists.map((watchlist) => {
+            {anime.map((watchlist) => {
               return (
                 <tr key={watchlist.id}>
                   <td>{watchlist.title}</td>
-                  <td>{watchlist.name}</td>
+                  <td>{watchlist.date}</td>
                   <td>
                     <img
                       src={watchlist.img_url}
                       alt={watchlist.title}
-                      width="20%"
-                      height="20%"
+                      width="50%"
+                      height="50%"
                     />
                   </td>
                   <td>
                     <button
                       className="btn btn-danger"
-                      onClick={() => delWatchlist(watchlist.id)}
+                      onClick={() => removeWatchlist(watchlist.id)}
                     >
-                      Cancel
+                      Delete
                     </button>
                   </td>
                 </tr>
@@ -101,6 +111,4 @@ const Watchlist = () => {
         </table>
       </>
     );
-  };
-};
-export default Watchlist;
+}
